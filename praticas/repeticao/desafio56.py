1a6edd88d40a7fd947eb8c87a554594ba4eda246
IrvinPaludo <irvin.paludo@gmail.com>
2025-09-08 18:45:15 +0000

    while

--- praticas/repeticao/desafio56.py
+++ praticas/repeticao/desafio56.py
@@ -28,6 +28,6 @@ for c in range (1,5):
 print(f'A média da idade é {mediaidade}')
 print(f'O homem mais velho é {maisvelho}')
 if mulnovas ==1:
-    print(f'A {mulnovas} mulher menore de 20 anos')
+    print(f'A {mulnovas} mulher menor de 20 anos')
 else:
     print(f'A {mulnovas} mulheres menores de 20 anos')
