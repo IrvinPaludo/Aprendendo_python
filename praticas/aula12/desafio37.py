99afa2e1c1d5452a166640ccf38dddb9a86b049b
IrvinPaludo <irvin.paludo@gmail.com>
2026-02-16 13:52:35 +0000

    desafio 37

--- praticas/aula12/desafio37.py
+++ praticas/aula12/desafio37.py
@@ -1,7 +1,21 @@
 
-num=int(input('Digite um nuúmero: '))
+num=int(input('Digite um nuúmero inteiro : '))
 print('Para qual base você quer converter:\n' \
-'1-Binario\n' \
-'2-Octal\n' \
-'3-hexadecimal')
-m= int(input(''))
+'[1] Binario\n' \
+'[2] Octal\n' \
+'[3] Hexadecimal')
+while True:
+    m= int(input(''))
+    if m == 1:
+        num = bin(num)
+        break        
+    elif m == 2:
+        num = oct(num)
+        break
+    elif m == 3:
+        num = hex(num)
+        break
+    else :
+        print('valor invalido \nTente de novo')
+        
+print(f'O valor ficou: {num [2:]}')
--- praticas/aula13/desafio50.py
+++ praticas/aula13/desafio50.py
@@ -4,7 +4,7 @@ for c in range(1,7):
     n = int(input('Digite o {}º número: '.format(c)))   
     if n % 2 == 0:
         soma += n
-if soma !=0:
+if soma != 0:
     print(f'A soma dos números pares é: {soma}')
 else:
     print('você não digitou numeros pares')
