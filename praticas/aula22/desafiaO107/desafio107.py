23a85c501830e18023128a414831f5053d445c6e
IrvinPaludo <irvin.paludo@gmail.com>
2026-04-29 19:10:34 +0000

    desafio 107

---
+++ praticas/aula22/desafiaO107/desafio107.py
@@ -0,0 +1,7 @@
+import uteis
+
+n = int(input('Digite um numero: '))
+print(f'A metade de {n} é {uteis.metade(n)}')
+print(f'O Dobro de {n} é {uteis.dobro(n)}')
+print(f'Aumentando 10%, temos {uteis.aumentar(n,10)}')
+print(f'Reduzindo 13%, temos {uteis.reduzir(n,13)}')
---
+++ praticas/aula22/desafiaO107/uteis.py
@@ -0,0 +1,18 @@
+
+def aumentar(numero,p):
+    resultado = (numero*(p/100))+numero
+    return resultado
+
+def reduzir(numero,p):
+    resultado = numero-(numero*(p/100))
+    return resultado
+
+def dobro(numero):
+    resultado = numero * 2
+    return resultado
+
+def metade(numero):
+    resultado = numero / 2
+    return resultado
+
+
